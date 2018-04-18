5e068caaa762f2f5108b05f76e6bd0d3c9ce0090
John Mount <jmount@win-vector.com>
2018-04-18 08:45:00 -0700

    add an if_else_op tree generator

--- R/if_else_block.R
+++ R/if_else_block.R
@@ -19,8 +19,12 @@
 #' is a reserved column name for this procedure.
 #'
 #' @param testexpr character containing the test expression.
+#' @param ... force later arguments to bind by name.
 #' @param thenexprs named character then assignments (altering columns, not creating).
 #' @param elseexprs named character else assignments (altering columns, not creating).
+#' @return sequence of statemens for extend_se().
+#'
+#' @seealso \code{\link{if_else_op}}
 #'
 #' @examples
 #'
@@ -48,10 +52,10 @@
 #'       qae(edited := 1)))
 #'   print(program)
 #'
-#'   trf <- extend_se(d, program)
-#'   cat(format(trf))
+#'   optree <- extend_se(d, program)
+#'   cat(format(optree))
 #'
-#'   sql <- to_sql(trf, my_db)
+#'   sql <- to_sql(optree, my_db)
 #'   cat(sql)
 #'
 #'   print(DBI::dbGetQuery(my_db, sql))
@@ -71,8 +75,11 @@
 #' @export
 #'
 if_else_block <- function(testexpr,
+                          ...,
                           thenexprs = NULL,
                           elseexprs = NULL) {
+  wrapr::stop_if_dot_args(substitute(list(...)),
+                          "rquery::if_else_block")
   if((length(thenexprs) + length(elseexprs))<=0) {
     return(NULL)
   }
@@ -118,4 +125,80 @@ if_else_block <- function(testexpr,
   program
 }
 
+
+#' Simulate a per-row block-\code{if(){}else{}}.
+#'
+#' This device uses expression-\code{ifelse(,,)} to simulate the
+#' more powerful per-row block-\code{if(){}else{}}.  The difference is
+#' expression-\code{ifelse(,,)} can choose per-row what value to express,
+#' whereas block-\code{if(){}else{}} can choose per-row where to assign multiple
+#' values. By simulation we mean: a sequence of quoted mutate expressions
+#' are emitted that implement the transform.  These expressions can then
+#' be optimized into a minimal number of no-dependency
+#' blocks by \code{\link{extend_se}} for efficient execution.
+#' The idea is the user can write legible code in this notation, and
+#' the translation turns it into safe and efficient code suitable for
+#' execution either on \code{data.frame}s or at a big data scale using
+#' \code{RPostgreSQL} or \code{sparklyr}.
+#'
+#' Note: \code{ifebtest_*}
+#' is a reserved column name for this procedure.
+#'
+#' @param source optree relop node or data.frame.
+#' @param testexpr character containing the test expression.
+#' @param ... force later arguments to bind by name.
+#' @param thenexprs named character then assignments (altering columns, not creating).
+#' @param elseexprs named character else assignments (altering columns, not creating).
+#' @return operator tree.
+#'
+#' @seealso \code{\link{if_else_block}}
+#'
+#' @examples
+#'
+#' if (requireNamespace("RSQLite", quietly = TRUE)) {
+#'   # Example: clear one of a or b in any row where both are set.
+#'   my_db <- DBI::dbConnect(RSQLite::SQLite(), ":memory:")
+#'   d <- dbi_copy_to(
+#'     my_db,
+#'     'd',
+#'     data.frame(i = c(1, 2, 3, 4, 5, 6, 7, 8, 9, 10),
+#'                a = c(0, 0, 1, 1, 1, 1, 1, 1, 1, 1),
+#'                b = c(0, 1, 0, 1, 1, 1, 1, 1, 1, 1),
+#'                edited = NA),
+#'     temporary=TRUE, overwrite=TRUE)
+#'
+#'   optree <- d %.>%
+#'     if_else_op(.,
+#'                testexpr = qe((a+b)>1),
+#'                thenexprs = qae(a := 0,
+#'                                b := 0,
+#'                                edited := 1),
+#'                elseexprs = qae(edited := 0))
+#'   cat(format(optree))
+#'
+#'   sql <- to_sql(optree, my_db)
+#'   cat(sql)
+#'
+#'   print(DBI::dbGetQuery(my_db, sql))
+#'
+#'   DBI::dbDisconnect(my_db)
+#' }
+#'
+#'
+#' @export
+#'
+if_else_op <- function(source,
+                       testexpr,
+                       ...,
+                       thenexprs = NULL,
+                       elseexprs = NULL) {
+  wrapr::stop_if_dot_args(substitute(list(...)),
+                          "rquery::if_else_op")
+  steps <- if_else_block(testexpr = testexpr,
+                         thenexprs = thenexprs,
+                         elseexprs = elseexprs)
+  source %.>%
+    extend_se(., steps) %.>%
+    drop_columns(., "ifebtest_1")
+}
 
