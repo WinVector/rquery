73cf1befcb299ac8e355109464b62ad05ed399b8
John Mount <jmount@win-vector.com>
2018-06-15 17:47:25 -0700

    add bad parse example

---
+++ tests/testthat/test_parse_issue.R
@@ -0,0 +1,18 @@
+
+library("rquery")
+context("parse_issue")
+
+
+
+test_that("test_parse_issue: Works As Expected", {
+  badex <- wrapr::build_frame(
+    'name'                      , 'expr'                                                              |
+    'Var228_lev_x.F2FyR07IdsN7I', 'ifelse(is.na(Var228), 0, ifelse(Var228 == "F2FyR07IdsN7I", 1, 0))' )
+  expr <- badex$expr
+  names(expr) <- badex$name
+  dat <- mk_td("tab", paste0("Var228"))
+  op <- extend_se(dat, expr)
+  str <- format(op) # notice some lines have more than 1 := !
+  parts <- strsplit(str, ":=", fixed = TRUE)
+  expect_equal(length(parts), 2)
+})
