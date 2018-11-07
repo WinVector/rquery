753c0d8cd18bdb6b4469e51a7b80a8e51b044bd5
John Mount <jmount@win-vector.com>
2018-11-06 17:18:27 -0800

    check for DBI before testing against it

--- tests/testthat/test_if_else.R
+++ tests/testthat/test_if_else.R
@@ -15,32 +15,34 @@ test_that("test_extend_partition: Works As Expected", {
 
 
 
+  if (requireNamespace("RSQLite", quietly = TRUE) &&
+      requireNamespace("DBI", quietly = TRUE)) {
+    my_db <- DBI::dbConnect(RSQLite::SQLite(),
+                            ":memory:")
 
-  my_db <- DBI::dbConnect(RSQLite::SQLite(),
-                          ":memory:")
+    d <- data.frame(id = 1:3, x = c(0, 1, NA))
+    # ifelse(d$x == 1, 'one', 'not one')
 
-  d <- data.frame(id = 1:3, x = c(0, 1, NA))
-  # ifelse(d$x == 1, 'one', 'not one')
+    d_remote <- rq_copy_to(my_db, "d_remote", d)
 
-  d_remote <- rq_copy_to(my_db, "d_remote", d)
+    optree <- d_remote %.>%
+      extend_nse(.,
+                 res = ifelse(x == 1, 'one', 'not one')) %.>%
+      orderby(., "id")
 
-  optree <- d_remote %.>%
-    extend_nse(.,
-               res = ifelse(x == 1, 'one', 'not one')) %.>%
-    orderby(., "id")
+    res <- execute(my_db, optree)
 
-  res <- execute(my_db, optree)
+    expect <- build_frame(
+      "id", "x", "res"     |
+        1L  , 0  , "not one" |
+        2L  , 1  , "one"     |
+        3L  , NA ,  NA       )
 
-  expect <- build_frame(
-    "id", "x", "res"     |
-    1L  , 0  , "not one" |
-    2L  , 1  , "one"     |
-    3L  , NA ,  NA       )
-
-  expect_equal(expect, res)
+    expect_equal(expect, res)
 
 
 
-  DBI::dbDisconnect(my_db)
+    DBI::dbDisconnect(my_db)
+  }
 
 })
--- tests/testthat/test_names.R
+++ tests/testthat/test_names.R
@@ -4,59 +4,62 @@ context("namedetection")
 
 
 test_that("test_names: Works As Expected", {
-  db <- DBI::dbConnect(RSQLite::SQLite(),
-                       ":memory:")
-  hdl <- rq_copy_to(db, "mtcars", mtcars,
-                     overwrite = TRUE,
-                     temporary = TRUE)
+  if (requireNamespace("RSQLite", quietly = TRUE) &&
+      requireNamespace("DBI", quietly = TRUE)) {
+    db <- DBI::dbConnect(RSQLite::SQLite(),
+                         ":memory:")
+    hdl <- rq_copy_to(db, "mtcars", mtcars,
+                      overwrite = TRUE,
+                      temporary = TRUE)
 
-  # check values don't look unbound
-  x <- "xval"
-  optree <- hdl %.>%
-    extend_nse(.,
-               xcol = x)
+    # check values don't look unbound
+    x <- "xval"
+    optree <- hdl %.>%
+      extend_nse(.,
+                 xcol = x)
 
-  # check a few ops
-  optree <- hdl %.>%
-    project_nse(.,
-                mean_mpg = avg(mpg),
-                groupby = "cyl")
-  expect_equal(qc(cyl, mpg),
-               sort(columns_used(optree)$mtcars))
-  expect_equal(qc(cyl, mean_mpg),
-               sort(column_names(optree)))
-  d <- execute(db, optree)
-
-  # detect unbound column reference
-  expect_error(
-    badtree <- hdl %.>%
+    # check a few ops
+    optree <- hdl %.>%
       project_nse(.,
-                  delay = avg(dep_time),
-                  groupby = "mpg")
-  )
+                  mean_mpg = avg(mpg),
+                  groupby = "cyl")
+    expect_equal(qc(cyl, mpg),
+                 sort(columns_used(optree)$mtcars))
+    expect_equal(qc(cyl, mean_mpg),
+                 sort(column_names(optree)))
+    d <- execute(db, optree)
 
-  # detect failure to assign
-  expect_error(
-    badtree <- hdl %.>%
+    # detect unbound column reference
+    expect_error(
+      badtree <- hdl %.>%
+        project_nse(.,
+                    delay = avg(dep_time),
+                    groupby = "mpg")
+    )
+
+    # detect failure to assign
+    expect_error(
+      badtree <- hdl %.>%
+        extend_nse(.,
+                   mpg + 1)
+    )
+
+    # detect non-scalar constant
+    v <- c(1, 2)
+    expect_error(
+      badtree <- hdl %.>%
+        extend_nse(.,
+                   d2 = mpg + v)
+    )
+
+    # make sure LHS do not drift and columns come first
+    d2 <- "zz"
+    mpg <- "zz"
+    p <- hdl %.>%
       extend_nse(.,
-                 mpg + 1)
-  )
-
-  # detect non-scalar constant
-  v <- c(1, 2)
-  expect_error(
-    badtree <- hdl %.>%
-      extend_nse(.,
-                  d2 = mpg + v)
-  )
-
-  # make sure LHS do not drift and columns come first
-  d2 <- "zz"
-  mpg <- "zz"
-  p <- hdl %.>%
-    extend_nse(.,
-               d2 = mpg)
-  expect_equal(sort(qc(am, carb, cyl, d2, disp, drat, gear, hp, mpg, qsec, vs, wt)),
-               sort(column_names(p)))
-  DBI::dbDisconnect(db)
+                 d2 = mpg)
+    expect_equal(sort(qc(am, carb, cyl, d2, disp, drat, gear, hp, mpg, qsec, vs, wt)),
+                 sort(column_names(p)))
+    DBI::dbDisconnect(db)
+  }
 })
