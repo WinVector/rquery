40acf6eab3f744735cf27e5d4d2f1b727bd25844
John Mount <jmount@win-vector.com>
2018-11-29 08:50:59 -0800

    test set_indicator

---
+++ tests/testthat/test_set_indicator.R
@@ -0,0 +1,37 @@
+
+library("rquery")
+
+
+context("test_set_indicator.R")
+
+
+test_that("test_set_indicator.R: Works As Expected", {
+
+  if (requireNamespace("DBI", quietly = TRUE) && requireNamespace("RSQLite", quietly = TRUE)) {
+    my_db <- DBI::dbConnect(RSQLite::SQLite(),
+                            ":memory:")
+
+    d <- rq_copy_to(my_db, 'd',
+                    data.frame(
+                      id = 1:4,
+                      a = c("1", "2", "1", "3"),
+                      b = c("1", "1", "3", "2"),
+                      q = 1,
+                      stringsAsFactors = FALSE),
+                    temporary = TRUE,
+                    overwrite = TRUE)
+    # example
+    set <- c("1", "2")
+    op_tree <- d %.>%
+      set_indicator(., "one_two", "a", set) %.>%
+      set_indicator(., "z", "a", c()) %.>%
+      orderby(., "id")
+    res = execute(my_db, op_tree)
+    testthat::expect_equivalent(c(1,1,1,0), res$one_two)
+
+    # cleanup
+    DBI::dbDisconnect(my_db)
+  }
+
+
+})
