bd669e03f7568f11f8a9171ae391c09ad1d85b7a
John Mount <jmount@win-vector.com>
2019-09-14 18:51:57 -0700

    check more and pass environments

--- R/yaml.R
+++ R/yaml.R
@@ -9,23 +9,34 @@ expr_map_to_expr_array <- function(emap) {
 #' Convert a series of simple objects (from YAML deserializaton) to an rquery pipeline.
 #'
 #' @param rep input objects
+#' @param ... not use, force later arguments to bind by name
 #' @param source input rquery node
+#' @param env environment to evaluate in
 #' @return rquery operator tree
 #'
 #' @export
 #'
-convert_yaml_to_pipeline <- function(rep, source=NULL) {
+convert_yaml_to_pipeline <- function(rep, ..., source=NULL, env = parent.frame()) {
+  force(env)
+  wrapr::stop_if_dot_args(substitute(list(...)),
+                          "rquery::convert_yaml_to_pipeline")
   if(is.null(names(rep))) {
+    if(!is.null(source)) {
+      stop("Expected source to be NULL")
+    }
     # unnamed list, a pipeline
-    res <- convert_yaml_to_pipeline(rep[[1]])
+    res <- convert_yaml_to_pipeline(rep[[1]], env = env)
     for(i in seqi(2, length(rep))) {
-      res <- convert_yaml_to_pipeline(rep[[i]], source=res)
+      res <- convert_yaml_to_pipeline(rep[[i]], source = res, env = env)
     }
     return(res)
   } else {
     # named list, a stage
     op = rep$op
     if(op=="TableDescription") {
+      if(!is.null(source)) {
+        stop("Expected source to be NULL")
+      }
       return(mk_td(table_name = rep$table_name,
                    columns = rep$column_names))
     }
@@ -52,7 +63,7 @@ convert_yaml_to_pipeline <- function(rep, source=NULL) {
       return(select_columns(source, columns=rep$columns))
     }
     if(op=="DropColumns") {
-      return(select_columns(source, drops = rep$column_deletions, strict = FALSE))
+      return(drop_columns(source, drops = rep$column_deletions, strict = FALSE))
     }
     if(op=="Rename") {
       return(rename_columns(source, cmap=rep$column_remapping))  # TODO: see if map is dumped (or __repr__() call wrote a string)
@@ -61,7 +72,7 @@ convert_yaml_to_pipeline <- function(rep, source=NULL) {
       return(orderby(source, cols=rep$order_columns, reverse=rep$reverse, limit=rep$limit))
     }
     if(op=="NaturalJoin") {
-      b = convert_yaml_to_pipeline(rep$b)
+      b = convert_yaml_to_pipeline(rep$b, env = env)
       return(natural_join(a = source, b = b,
                           by = rep$by, jointype = toupper(rep$jointype)))
     }
